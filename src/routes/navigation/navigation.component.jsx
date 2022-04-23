2fd90b18fc62aaecc8b7fd7774ee2d2826182037
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-04-23 22:33:45 +0200

    add Sign-In component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,23 +1,27 @@
-import {Fragment} from 'react'
-import {Outlet, Link} from 'react-router-dom'
-import {ReactComponent as CrownLogo} from '../../assets/crownLogo.svg'
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as CrownLogo } from '../../assets/crownLogo.svg'
 import './navigation.styles.scss'
 
-const Navigation =()=>{
-    return(
-      <Fragment>
-        <div className="navigation">
-            <Link className="logo-container" to='/'>
-                <CrownLogo className="logo" />
-            </Link>
-          <div className="nav-links-container"></div>
-            <Link className="nav-link" to='/shop'>
-            Shop
-            </Link>
-        </div>
-        <Outlet/>
-      </Fragment>
-      )
-  }
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to='/'>
+                    <CrownLogo className="logo" />
+                </Link>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to='/shop'>
+                        Shop
+                    </Link>
+                    <Link className="nav-link" to='/sign-in'>
+                        SIGN IN
+                    </Link>
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
 
-  export default Navigation
+export default Navigation
