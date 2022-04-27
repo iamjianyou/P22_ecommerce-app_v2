61202bc34b94adb74bbe40dac553357d260b50a8
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-04-27 21:43:59 +0200

    authentication component styling

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,6 +4,8 @@ import {
 } from '../../Utils/firebase/firebase.utils'
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component';
+import './authentication.styles.scss'
+
 const Authentication = () => {
     const logGoogleUser = async () => {
         const {user} = await signInwithGooglePopup();
@@ -11,9 +13,7 @@ const Authentication = () => {
     }
 
     return (
-        <div>
-            <h1> Sign In Page</h1>
-            <button onClick={logGoogleUser}> Sign in with Google Popup </button>
+        <div className="authentication-container">
             <SignInForm/>
             <SignUpForm/>
         </div>
