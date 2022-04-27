31270256620caf84969a15f9f591ad6ccc6b88be
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-04-27 15:15:55 +0200

    rename sign-in to authencation, auth

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,21 @@
+import { 
+    signInwithGooglePopup,
+    createUserDocumentFromAuth,
+} from '../../Utils/firebase/firebase.utils'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
+const Authentication = () => {
+    const logGoogleUser = async () => {
+        const {user} = await signInwithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    }
+
+    return (
+        <div>
+            <h1> Sign In Page</h1>
+            <button onClick={logGoogleUser}> Sign in with Google Popup </button>
+            <SignUpForm/>
+        </div>
+    )
+}
+export default Authentication
