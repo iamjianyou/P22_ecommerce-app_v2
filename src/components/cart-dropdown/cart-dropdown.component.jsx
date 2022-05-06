9953c1d65fcb4f5658da49f83c3253a700203c48
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-05-06 15:04:41 +0200

    create cart dropdown component

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,18 @@
+import './cart-dropdown.styles.scss'
+import Button from '../button/button.component';
+const CartDropdown = () => {
+
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items'/>
+                <Button> Go Checkout </Button>
+
+        </div>
+    )
+    
+}
+
+export default CartDropdown
+
+
+
