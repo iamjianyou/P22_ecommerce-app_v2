0d0247fcf96fc31f1833af022fefb7a097d96a24
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-05-06 14:00:39 +0200

    create cart-card, get data by productContext, styling

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,17 +1,23 @@
 // import SHOP_DATA from '../../shop-data.json'
 import { useContext } from 'react'
 import { ProductsContext } from '../../contexts/product.context'
+import ProductCard from '../../components/product-card/product-card.component'
+import './shop.styles.scss'
 
 const Shop = () => {
     const {products} = useContext(ProductsContext)
     console.log('products -> ', products)
     return (
-        <div>
-            {products.map(({id, name})=>(
+        <div className='products-container'>
+            {products.map((product)=> (
+                <ProductCard key={product.id} product={product} />
+            ))}
+            
+            {/* {products.map(({id, name})=>(
                 <div key={id}>
                     <h1> {name} </h1>
                 </div>
-            ) )}
+            ) )} */}
         </div>
     )
 }
