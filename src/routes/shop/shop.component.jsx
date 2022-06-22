6f6aa506f25b94b08aff9c546354c357a7696ed5
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-06-22 22:43:59 +0200

    category preview component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,28 +1,34 @@
 // import SHOP_DATA from '../../shop-data.json'
 import { useContext, Fragment} from 'react'
 import { CategoriesContext } from '../../contexts/categories.context'
-import ProductCard from '../../components/product-card/product-card.component'
+
+import CategoryPreview from '../../components/category-preview/category-preview.component'
+
 import './shop.styles.scss'
 
 const Shop = () => {
     const {categoriesMap} = useContext(CategoriesContext)
     console.log('categoriesMap ----> ', categoriesMap)
     return (
-        <Fragment>
-        {Object.keys(categoriesMap).map(title => (
-                <Fragment key={title}>
-                    <h2> {title} </h2>
-                    <div className='products-container'>
-                        {categoriesMap[title].map((product)=> (
-                            <ProductCard key={product.id} product={product} />
-                        ))}
+        <div className="shop-container">
+        {Object.keys(categoriesMap).map(title => {
 
-                    </div>
-                </Fragment>
+            const products = categoriesMap[title];
+            return <CategoryPreview key={title} title={title} products={products} />
+
+                // <Fragment key={title}>
+                //     <h2> {title} </h2>
+                //     <div className='products-container'>
+                //         {categoriesMap[title].map((product)=> (
+                //             <ProductCard key={product.id} product={product} />
+                //         ))}
+
+                //     </div>
+                // </Fragment>
                 
-            ))}
+        })}
 
-        </Fragment>
+        </div>
     )
 }
 
