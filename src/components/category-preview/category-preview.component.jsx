0030784189f9db2e20cc0b1652011965804010cf
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-06-29 15:34:12 +0200

    add category  style and link

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,5 +1,6 @@
 import ProductCard from '../product-card/product-card.component'
 import './category-preview.styles.scss'
+import { Link } from 'react-router-dom'
 
 
 const CategoryPreview = ({title, products}) => {
@@ -7,7 +8,7 @@ const CategoryPreview = ({title, products}) => {
     return (
         <div className="category-preview-container">
             <h2>
-                <span className="title"> {title.toUpperCase()}</span>
+                <Link className="title" to={title}> {title.toUpperCase()}</Link>
 
             </h2>
             <div className="preview">
