4274eeffa838c1e9735a3de8e3bc8f93a26404a1
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-07-07 22:28:52 +0200

    implement with Reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,5 +1,8 @@
+/** To replace context with reducers 
+ *  And use reducers inside of context
+*/
 
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener, createUserDocumentFromAuth} from '../Utils/firebase/firebase.utils';
 
 // As the actual value to get access
@@ -7,9 +10,53 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
     currentUser: null,
   });
-  
+
+/** Action types */
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state, action ) => {
+  console.log('state--> ', state)
+  console.log('dispatched')
+  console.log('action -> ', action)
+  const {type, payload} = action;
+  // return back an object with those valuses depending on the type
+  // ...state => spread through the previous state in all of the current values for the object
+  switch(type){
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+         
+      }
+    default:
+      throw new Error(`Unhandled type: ${type} in userReducer`)
+  }
+}
+
+
+
+  /** Istead to use reducer inside of Context */
   export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    // to utilize the userReducer by using useReducer hook
+    // [state(currentUser), dispatch]
+    // current values thats being stored by the reducer
+    // its a fn that whenever you call it, you pass it an action object
+    const [{currentUser}, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    console.log('currentUser --> ', currentUser)
+
+    const setCurrentUser = (user) => {
+      dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user} )
+    }
+
+
+    // const [currentUser, setCurrentUser] = useState(null);
     const value = { currentUser, setCurrentUser };
   
     useEffect(() => {
@@ -25,4 +72,5 @@ export const UserContext = createContext({
   
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
   };
+
 
