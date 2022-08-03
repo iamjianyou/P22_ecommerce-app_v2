b756be960f762b655cd91a89c04319638363dab4
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-08-03 09:51:29 +0200

    practise of accessibility with Context(category categoryPreview in shop page)

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
-
+import { CategoriesContext } from '../../contexts/categories.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
@@ -14,6 +14,8 @@ import {
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const { categoriesMap } = useContext(CategoriesContext)
+  console.log(categoriesMap)
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
