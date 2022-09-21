5c57d793dcf2cd044ba2b4b26da76fcdea052c3c
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-09-22 01:29:23 +0200

    apply useDispatch

--- src/App.js
+++ src/App.js
@@ -2,29 +2,21 @@ import { useEffect} from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth} from './Utils/firebase/firebase.utils';
+import { getCurrentUser} from './Utils/firebase/firebase.utils';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component'
-import { setCurrentUser } from './store/user/user.action'
+import { checkUserSession } from './store/user/user.action'
 
 
 const App = () => {
-  const dispatch = useDispatch(); // works similar to user context dispatch
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      dispatch(setCurrentUser(user)); // (only one)dispatch actions to the root reducer, which inturn  pass the action to every single reducer function
-    });
-
-    return unsubscribe; // unsubscribe when unmounted
-  }, [dispatch]); // dep not change actually because it comes from the hooks
-
-   
+    dispatch(checkUserSession());
+  }, []);
 
   return (
     <Routes>
