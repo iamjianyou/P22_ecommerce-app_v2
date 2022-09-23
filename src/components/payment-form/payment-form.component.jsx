056b8c29145439c44a838f8a2b7ad7d3c0f7e5f6
Jianyou Dai <jianyou.dai@soprasteria.com>
2022-09-23 11:37:01 +0200

    finish the strip payment

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -8,14 +8,42 @@ const PaymentForm = () => {
 
     const paymentHandler = async (e) => {
         e.preventDefault();
+        if (!stripe || !elements) {
+          return;
+        }
 
-        if(!stripe || !elements) return;
+        if(!stripe || !elements) {return;}
         
-    
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({amount:100})
+        }).then(res => { return res.json()});
+        // console.log(response);
+        const {paymentIntent: {client_secret}} = response;
+        const paymentResult = await stripe.confirmCardPayment(client_secret, {
+            payment_method: {
+              card: elements.getElement(CardElement),
+              billing_details: {
+                name: 'Jianyou',
+              },
+            },
+          });
+
+       if (paymentResult.error){
+           alert(paymentResult.error); 
+       }else{
+        if(paymentResult.paymentIntent.status === 'succeeded'){
+            alert('Payment successfull')
+        }
+       }
     }
+   
     return (
         <PaymentFormContainer>
-            <FormContainer>
+            <FormContainer onSubmit={paymentHandler}>
                 <h2> Credit Card Payment:  </h2>
                 <CardElement />
                 <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
